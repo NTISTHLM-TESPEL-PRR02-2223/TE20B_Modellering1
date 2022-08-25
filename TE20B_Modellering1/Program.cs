0d40ea4f7f84fca816cac366c245b9f4cc1ee919
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-08-25 10:06:47 +0200

    Initial commit

---
+++ TE20B_Modellering1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Weapon w2;
+
+w2 = new Weapon();
+
+Console.WriteLine(w2.Ammo);
+
+w2.Ammo--;
+
+Console.WriteLine(w2.Ammo);
+
+Console.ReadLine();
+
---
+++ TE20B_Modellering1/Weapon.cs
@@ -0,0 +1,6 @@
+using System;
+
+public class Weapon
+{
+  public int Ammo = 100;
+}
